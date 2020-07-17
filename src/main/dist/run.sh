da2987e1e21939ae252b7c625ca9012fc3457d38
Logan Lamers <llamers@mcw.edu>
2020-07-17 10:13:44 -0500

    emails added

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -9,7 +9,7 @@ SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 
 EMAILLIST=llamers@mcw.edu,mtutaj@mcw.edu
 if [ "$SERVER" == "REED" ]; then
-  EMAILLIST=llamers@mcw.edu,mtutaj@mcw.edu,jrsmith@mcw.edu,jdepons@mcw.edu
+  EMAILLIST=llamers@mcw.edu,mtutaj@mcw.edu,jrsmith@mcw.edu,sjwang@mcw.edu
 fi
 
 cd $APPDIR
